f6d99ab0acd92bf576e8c199463fb6432bd8f566
amirzuev <mirzuev.a.v@gmail.com>
2019-01-04 04:42:08 +0300

    change age from 20 to 21

--- src/main/java/appdev/org/testcicd/TestJsonObjectController.java
+++ src/main/java/appdev/org/testcicd/TestJsonObjectController.java
@@ -16,7 +16,7 @@ public class TestJsonObjectController {
     public String TestJsonObjectController() {
         JSONObject json_obj = new JSONObject();
             json_obj.put("name", "Jone");
-            json_obj.put("age", 20);
+            json_obj.put("age", 21);
         return json_obj.toString();
     }
 }
