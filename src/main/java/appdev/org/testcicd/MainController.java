d2a6ec99d9cd2b270b94f42e790d7bd561cb0e50
amirzuev <mirzuev.a.v@gmail.com>
2019-01-04 16:48:34 +0300

    add MainController

--- src/main/java/appdev/org/testcicd/MainController.java
+++ src/main/java/appdev/org/testcicd/MainController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @RequestMapping("/")
     public String main(){
-        return "Hello World"
+        return "Hello World";
     }
 }
--- src/test/java/appdev/org/testcicd/LivnessProbeTest.java
+++ src/test/java/appdev/org/testcicd/LivnessProbeTest.java
@@ -1,42 +1,42 @@
 package appdev.org.testcicd;
 
 
-import org.junit.Assert;
-import org.junit.Before;
+import javafx.application.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockServletContext;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { ApplicationConfig.class} )
-@WebAppConfiguration
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = { TestCiCdApplication.class} )
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@WebAppConfiguration
+//@ContextConfiguration
+@AutoConfigureMockMvc
 public class LivnessProbeTest {
 
     @Autowired
-    private WebApplicationContext wac;
+    //private WebApplicationContext wac;
     private MockMvc mockMvc;
 
-    @Before
-    public void setup() throws Exception {
-        this.mockMvc = MockMvcBuilder.webAppContextSetup(this.wac).build();
-
-    }
-
     @Test
-    public void giveWac_whenServletContext_thenItProvidesGreetController() {
-        ServletContext servletContext = wac.getServletContext();
-        Assert.assertNotNull(servletContext);
-        Assert.assertTrue(servletContext instanceof MockServletContext);
-        Assert.assertNotNull(wac.getBean("greetController"));
+    public void Test01() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
     }
 
 
