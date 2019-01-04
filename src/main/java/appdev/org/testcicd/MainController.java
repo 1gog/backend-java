4a89fe6fbce9f87a3619f4448c61ca4e799fddf0
amirzuev <mirzuev.a.v@gmail.com>
2019-01-04 16:46:28 +0300

    add main controller

---
+++ src/main/java/appdev/org/testcicd/MainController.java
@@ -0,0 +1,13 @@
+package appdev.org.testcicd;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @RequestMapping("/")
+    public String main(){
+        return "Hello World"
+    }
+}
