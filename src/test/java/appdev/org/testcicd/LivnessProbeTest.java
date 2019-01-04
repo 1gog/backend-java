83746952d5ba29e8d9128b51a7e95dabd23a5fe6
amirzuev <mirzuev.a.v@gmail.com>
2019-01-04 05:23:00 +0300

    add in pom.xml test framework

--- src/test/java/appdev/org/testcicd/LivnessProbeTest.java
+++ src/test/java/appdev/org/testcicd/LivnessProbeTest.java
@@ -1,34 +1,44 @@
 package appdev.org.testcicd;
 
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Arrays;
-
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import org.springframework.test.web.servlet.MockMvc;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {TestContext.class, WebAppContext.class})
+@ContextConfiguration(classes = { ApplicationConfig.class} )
 @WebAppConfiguration
-
 public class LivnessProbeTest {
 
+    @Autowired
+    private WebApplicationContext wac;
     private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilder.webAppContextSetup(this.wac).build();
+
+    }
+
+    @Test
+    public void giveWac_whenServletContext_thenItProvidesGreetController() {
+        ServletContext servletContext = wac.getServletContext();
+        Assert.assertNotNull(servletContext);
+        Assert.assertTrue(servletContext instanceof MockServletContext);
+        Assert.assertNotNull(wac.getBean("greetController"));
+    }
+
 
 
 }
