01892502a40b7d85e6b55aea68c2855548afa29d
amirzuev <mirzuev.a.v@gmail.com>
2019-01-09 03:13:59 +0300

    add api/car, api/car/all, api/car/add

---
+++ src/main/java/appdev/org/testcicd/Car.java
@@ -0,0 +1,44 @@
+package appdev.org.testcicd;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Car {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String color;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+}
+
---
+++ src/main/java/appdev/org/testcicd/CarController.java
@@ -0,0 +1,39 @@
+package appdev.org.testcicd;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import appdev.org.testcicd.CarRepository;
+import appdev.org.testcicd.Car;
+
+
+
+@Controller
+@RequestMapping(path="/api/car")
+public class CarController {
+	
+	@Autowired
+	private CarRepository carRepository;
+
+	// save new car 
+	@GetMapping(path="/api/car/add")
+	public @ResponseBody String addNewCar(@RequestParam String name, @RequestParam String color ){
+
+		Car car = new Car();
+		car.setName(name);
+		car.setColor(color);
+		carRepository.save(car);
+		return "saved";
+	}
+
+	// get all car
+	@GetMapping(path="/api/car/all")
+	public @ResponseBody Iterable<Car> getAllUsers(){
+		return carRepository.findAll();
+	}
+}
+
